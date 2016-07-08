770e3079ab097aebba4e9fafc6e8929d22e422bf
alan bount <alan@zeroasterisk.com>
2016-07-08 15:13:21 -0400

    BUG: Accounts.setPassword() fails due to aws-sdk
    * if you install the npm `aws-sdk` but do not import it, no problem
    * if you import `aws-sdk` but do not actually use it somewhere, BUG
    * if you import `aws-sdk` and use it (not shown) AWS works, but the Accounts BUG
    * if you require `aws-skd` (ES5) BUG
    
    It was a few hours to whittle down this bug to this cause.
    
    I do not know what inside of `aws-sdk` would be causing this problem...
    
    It might be only broken with recent versions of `aws-sdk`
    (unsure, I'll be testing a few soon, and will update if I find any working)
    
    It is likely an npm issue (?) but if it's happening for AWS, it's important,
    and perhaps indicative of a broader issue.

--- server/main.js
+++ server/main.js
@@ -1,1 +1,25 @@
 import '../imports/api/tasks.js';
+
+// -------------------------------------------
+// BUG: Accounts.setPassword() fails when aws-sdk is imported
+// -------------------------------------------
+// If you comment this out all of these imports, it works fine
+// import AWS from 'aws-sdk';
+
+// I have tried these methods of importing as well
+// import { S3 } from 'aws-sdk';
+// const AWS = require('aws-sdk');
+
+// Super basic inline testing
+Meteor.startup(() => {
+  // Super basic inline testing
+  console.log('BUG: Accounts.setPassword() fails when aws-sdk is imported');
+  if (Meteor.users.find().count() === 0) {
+    console.log('No Users -- creating mock user');
+    Accounts.createUser({username: 'mock', email: 'mock@mock.com', password: 'mock'});
+  }
+  const user = Meteor.users.findOne();
+  console.log(`Setting Password on User: ${user._id}`);
+  Accounts.setPassword(user._id, 'newpassword');
+  console.log('SUCCESS');
+});
